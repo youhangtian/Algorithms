7b54a39b13fa3f5d926d5552006966bf8798560d
youhangtian <1013515028@qq.com>
2015-11-25 13:09:18 +0800

    Create codejam.cpp

---
+++ codejam.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <functional>
+#include <climits>
+#include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < n; i ++)
+#define mem(s, v) memset(s, v, sizeof(s))
+#define sz(v) (int)v.size()
+#define pb push_back
+#define mp make_pair
+
+int main() {
+//    freopen("/Users/youhangtian/Downloads/D-large-practice.in.txt", "r", stdin);
+//    freopen("/Users/youhangtian/Downloads/output.txt", "w", stdout);
+    
+    int T;
+    cin >> T;
+    
+    for (int ca = 1; ca <= T; ca ++) {
+        
+    }
+    
+    return 0;
+}
